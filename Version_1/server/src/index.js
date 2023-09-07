e31e8fed1fa41a54f56de14fea911d3972a697c0
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-07 13:08:12 -0700

    finish basic imports

--- Version_1/server/src/index.js
+++ Version_1/server/src/index.js
@@ -1,1 +1,4 @@
-import express from 'express'
+import express from 'express' //serve as framework to create api. Serves frontend
+import cors from 'cors' //libary, sets up rules for frontend and backend
+import mongoose from 'mongoose' //write queries to database
+
