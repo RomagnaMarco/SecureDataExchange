1736ea570387bf9c5d2faea2959d96dc6b8cee2a
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-07 13:14:28 -0700

    set up some configs

--- Version_1/server/src/index.js
+++ Version_1/server/src/index.js
@@ -1,4 +1,18 @@
-import express from 'express' //serve as framework to create api. Serves frontend
-import cors from 'cors' //libary, sets up rules for frontend and backend
-import mongoose from 'mongoose' //write queries to database
+// Importing necessary libraries
+import express from 'express';      // Express framework for building the API
+import cors from 'cors';            // Middleware for enabling cross-origin resource sharing
+import mongoose from 'mongoose';    // Library for MongoDB object modeling
 
+// Initializing the express application
+const app = express();
+
+// Middleware configurations
+
+// Enable JSON body parsing for incoming requests (to read JSON payload from frontend)
+app.use(express.json());
+
+// Enable CORS with default settings (allows requests from any origin by default)
+app.use(cors());
+
+// Start the server on port 3001
+app.listen(3001, () => console.log("Server Started on Port 3001"));
