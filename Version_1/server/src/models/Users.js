fddff0fa28ebb72c3ec613a48cdee91027bf6fab
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-08 01:02:58 -0700

    handled export, added comments

--- Version_1/server/src/models/Users.js
+++ Version_1/server/src/models/Users.js
@@ -1,6 +1,13 @@
-import mongoose from "mongoose"
+// Importing the mongoose library for MongoDB object modeling
+import mongoose from "mongoose";
 
+// Defining a schema for the User collection in MongoDB
+// The schema describes the structure of the documents in the collection
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-})
+});
+
+// Creating a UserModel using the UserSchema which corresponds to the "users" collection in MongoDB
+// Any operations on the "users" collection will use this model
+export const UserModel = mongoose.model("users", UserSchema);
