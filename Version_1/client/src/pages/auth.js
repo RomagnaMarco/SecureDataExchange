1f23eb089cf48ea937c19dbf5f3def5da17eb40c
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-11 15:01:06 -0700

    connecting backend route to frontend

--- Version_1/client/src/pages/auth.js
+++ Version_1/client/src/pages/auth.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import axios from 'axios'
 
 // The auth component definition
 export const Auth = () => {
@@ -29,20 +30,43 @@ const Register = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            await axios.post("http://localhost:3001/auth/register", {
+                username,
+                password,
+            })
+            alert("Registration Completed! Now Login.")
+        } catch (err) {
+            alert("An error has occured during registration")
+            console.error(err)
+        }
+
+    }
+
     return (<Form 
         username={username} 
         setUsername={setUsername} 
         password={password} 
         setPassword={setPassword}
         label="Register"
+        onSubmit={onSubmit}
     />
     )
 }
 
-const Form = ({username, setUsername, password, setPassword, label}) => {
+const Form = ({
+    username,
+    setUsername,
+    password,
+    setPassword, 
+    label, 
+    onSubmit 
+}) => {
     return (
         <div className="auth-container">
-            <form>
+            <form onSubmit={onSubmit}>
                 <h2> {label} </h2>
                 <div className="form-group">
                     <label htmlFor="username"> Username: </label>
@@ -56,13 +80,13 @@ const Form = ({username, setUsername, password, setPassword, label}) => {
                 <div className="form-group">
                     <label htmlFor="password"> Password: </label>
                     <input 
-                        type="text" // look into changing this to type="password" for security
+                        type="password"
                         id="password" 
                         value={ password }
                         onChange={(event) => setPassword(event.target.value)}
                         />
                 </div>
-                <button type="submit"> Register </button>
+                <button type="submit"> {label} </button>
             </form>
         </div>
     );
