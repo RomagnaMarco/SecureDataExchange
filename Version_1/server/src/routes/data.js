4f967fa5defb8ca1e0a3870c232cf69aa4291eac
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-11 16:05:35 -0700

    added basic functionality to get data.

--- Version_1/server/src/routes/data.js
+++ Version_1/server/src/routes/data.js
@@ -4,4 +4,13 @@ import { DataModel } from "../models/data.js";
 
 const router = express.router()
 
+router.get("/", async (req, res) => {
+    try {
+        const response = await RecipeModel.find({ clearanceLevel: 0 }) //finds all data with level 0 clearance
+        res.json(response)
+    } catch (err) {
+        res.json(err)
+    }
+})
+
 export { router as dataRouter }
