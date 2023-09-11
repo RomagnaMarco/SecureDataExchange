8f25bca68ad295a3fa30b892af3e9e4f6ba77090
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-10 18:27:39 -0700

    added routing to frontend

--- Version_1/client/src/App.js
+++ Version_1/client/src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Home } from './pages/home';
+import { SavedData } from './pages/saved-data';
+import { AddData } from './pages/add-data';
+import { Auth } from './pages/auth';
+
 
 function App() {
   return (
-    <div className="App"> </div>
+    <div className="App"> 
+        <Router>
+            <Routes>
+                <Route path="/" element={ <Home />} />
+                <Route path="/auth" element={ <Auth />} />
+                <Route path="/add-data" element={ <AddData />} />
+                <Route path="/saved-data" element={ <SavedData />} />
+            </Routes>
+        </Router>
+    </div>
   );
 }
 
