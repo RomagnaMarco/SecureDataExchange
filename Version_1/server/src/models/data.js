c4cd136322232246c40053e6e4c0f5f1b6e039f9
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-11 15:53:51 -0700

    added data basics

--- Version_1/server/src/models/data.js
+++ Version_1/server/src/models/data.js
@@ -1,1 +1,19 @@
-import mongoose from "mongoose"
+// Importing the mongoose library for MongoDB object modeling.
+import mongoose from "mongoose";
+
+// Defining a schema for the Data collection in MongoDB.
+// The schema describes the structure of the documents within this collection.
+const DataSchema = new mongoose.Schema({              
+    clearanceLevel: {type: Number, required: true},
+    info: [{ type: String, required: false }],
+    userOwner: { 
+        type: mongoose.Schema.Types.ObjectId,         // Using ObjectId to reference a user document.
+        ref: "users",                                // Establishing a reference to the 'users' collection.
+        required: true                               // Mandatory field to track the original user who added the data.
+    }
+});
+
+// Creating a model named 'data' based on the DataSchema.
+// This model represents the 'data' collection in MongoDB.
+// We'll use this model for CRUD operations on the 'data' collection.
+export const DataModel = mongoose.model("data", DataSchema);
