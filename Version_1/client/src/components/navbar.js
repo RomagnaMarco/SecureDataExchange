1f2c33ec3686121597ae14114e8e2e176c58110a
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-11 15:21:04 -0700

    added logout, based on token, cookies.

--- Version_1/client/src/components/navbar.js
+++ Version_1/client/src/components/navbar.js
@@ -1,13 +1,21 @@
 import { Link} from 'react-router-dom'
+import { useCookies } from "react-cookie"
 
 
 export const Navbar = () => {
+
+    const [cookies, setCookies] = useCookies(["access_token"])
+
     return (
     <div className ="navbar"> 
     <Link to="/"> Home </Link>{" "}
     <Link to="/add-data"> Add Data </Link>{" "}
     <Link to="/saved-data"> Saved Data </Link>{" "}
-    <Link to="/auth"> Login/Register </Link>{" "}
+    {!cookies.access_token ? (
+        <Link to="/auth"> Login/Register </Link>
+        ) : (
+            <button> Logout </button>
+        )}
     </div>
     )
 }
