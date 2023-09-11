e0d9ceeb53ff6c2d3e043a386e38a692a270f907
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-11 15:24:31 -0700

    added logout functionality

--- Version_1/client/src/components/navbar.js
+++ Version_1/client/src/components/navbar.js
@@ -1,10 +1,20 @@
 import { Link} from 'react-router-dom'
 import { useCookies } from "react-cookie"
+import { useNavigate } from 'react-router-dom'
 
 
 export const Navbar = () => {
 
     const [cookies, setCookies] = useCookies(["access_token"])
+
+    //navigate hook
+    const navigate = useNavigate()
+
+    const logout = () => {
+        setCookies("access_token", "")
+        window.localStorage.removeItem("userID")
+        navigate("/auth")
+    }
 
     return (
     <div className ="navbar"> 
@@ -14,7 +24,7 @@ export const Navbar = () => {
     {!cookies.access_token ? (
         <Link to="/auth"> Login/Register </Link>
         ) : (
-            <button> Logout </button>
+            <button onClick={logout}> Logout </button>
         )}
     </div>
     )
