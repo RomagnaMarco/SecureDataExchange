33fc26df6b160d0c69cfe557d7b4bec1782db379
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-11 16:49:02 -0700

    add clearance level to webtoken

--- Version_1/server/src/routes/users.js
+++ Version_1/server/src/routes/users.js
@@ -53,7 +53,10 @@ router.post("/login", async (req, res) => {
         return res.json({message: "Username or Password is Incorrect! "})
 
     //make a webtoken
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({
+         id: user._id,
+         clearanceLevel: user.clearanceLevel
+    }, process.env.JWT_SECRET);
     res.json({token, userID: user._id })
 
 });
