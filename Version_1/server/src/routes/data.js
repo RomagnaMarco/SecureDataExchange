3615604e36d24178ae16617dbea9a7b477b85cdb
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-11 16:41:36 -0700

    added put for data

--- Version_1/server/src/routes/data.js
+++ Version_1/server/src/routes/data.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { DataModel } from "../models/data.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router()
 
@@ -19,6 +20,19 @@ router.post("/", async (req, res) => {
     try {
         const response = await data.save()
         res.json(response)
+    } catch (err) {
+        res.json(err)
+    }
+})
+
+router.put("/", async (req, res) => {
+
+    try {
+        const data = await DataModel.findById(findbyID(req.body.dataID))
+        const user = await UserModel.findById(findbyID(req.body.userID))
+        user.savedData.push(data)
+        await data.save()
+        res.json({ savedData: user.savedData })
     } catch (err) {
         res.json(err)
     }
