63ab7c6693e9d17fabdc1fa85b4bd0c910d7d5af
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-10 18:56:23 -0700

    added logic for register

--- Version_1/client/src/pages/auth.js
+++ Version_1/client/src/pages/auth.js
@@ -1,5 +1,52 @@
+import { useState } from "react"
+
 // The auth component definition
 export const Auth = () => {
+    return (
+    <div className="auth">
+         <Login />
 
-    return <div> Auth </div>
+         <Register />
+    </div>
+    )
+}
+
+const Login = () => {
+    return (
+    <div className="auth-container">
+        
+    </div>
+    )
+}
+
+const Register = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+    <div className="auth-container">
+        <form>
+            <h2> Register </h2>
+            <div className="form-group">
+                <label htmlFor="username"> Username: </label>
+                <input
+                     type="text" 
+                     id="username"
+                     value = { username }
+                     onChange={(event) => setUsername(event.target.value)}
+                     />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password"> Password: </label>
+                <input 
+                    type="text" //explore type password later
+                    id="password" 
+                    value={ password }
+                    onChange={(event) => setPassword(event.target.value)}
+                    />
+            </div>
+            <button type="submit"> Register </button>
+        </form>
+    </div>
+    )
 }
