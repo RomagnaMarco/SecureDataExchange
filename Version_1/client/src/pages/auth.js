c4512adc008f5d9ca5da4d1de63077dceb7e3769
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-10 18:59:40 -0700

    simplied code for register

--- Version_1/client/src/pages/auth.js
+++ Version_1/client/src/pages/auth.js
@@ -12,14 +12,6 @@ export const Auth = () => {
 }
 
 const Login = () => {
-    return (
-    <div className="auth-container">
-        
-    </div>
-    )
-}
-
-const Register = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
@@ -49,4 +41,38 @@ const Register = () => {
         </form>
     </div>
     )
+}
+
+const Register = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    return <Form />
+}
+
+const Form = ({username, setUsername, password, setPassword}) => {
+    <div className="auth-container">
+        <form>
+            <h2> Register </h2>
+            <div className="form-group">
+                <label htmlFor="username"> Username: </label>
+                <input
+                     type="text" 
+                     id="username"
+                     value = { username }
+                     onChange={(event) => setUsername(event.target.value)}
+                     />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password"> Password: </label>
+                <input 
+                    type="text" //explore type password later
+                    id="password" 
+                    value={ password }
+                    onChange={(event) => setPassword(event.target.value)}
+                    />
+            </div>
+            <button type="submit"> Register </button>
+        </form>
+    </div>
 }
