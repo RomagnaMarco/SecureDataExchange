daaa2f0f1fe63fbff4e38008d34244e953627208
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-10 19:04:49 -0700

    fixed form, finished login basics

--- Version_1/client/src/pages/auth.js
+++ Version_1/client/src/pages/auth.js
@@ -15,31 +15,12 @@ const Login = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
-    return (
-    <div className="auth-container">
-        <form>
-            <h2> Register </h2>
-            <div className="form-group">
-                <label htmlFor="username"> Username: </label>
-                <input
-                     type="text" 
-                     id="username"
-                     value = { username }
-                     onChange={(event) => setUsername(event.target.value)}
-                     />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password"> Password: </label>
-                <input 
-                    type="text" //explore type password later
-                    id="password" 
-                    value={ password }
-                    onChange={(event) => setPassword(event.target.value)}
-                    />
-            </div>
-            <button type="submit"> Register </button>
-        </form>
-    </div>
+    return (<Form 
+        username={username} 
+        setUsername={setUsername} 
+        password={password} 
+        setPassword={setPassword} 
+    />
     )
 }
 
@@ -47,37 +28,40 @@ const Register = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
-    return <Form 
+    return (<Form 
         username={username} 
         setUsername={setUsername} 
         password={password} 
         setPassword={setPassword} 
     />
+    )
 }
 
 const Form = ({username, setUsername, password, setPassword}) => {
-    <div className="auth-container">
-        <form>
-            <h2> Register </h2>
-            <div className="form-group">
-                <label htmlFor="username"> Username: </label>
-                <input
-                     type="text" 
-                     id="username"
-                     value = { username }
-                     onChange={(event) => setUsername(event.target.value)}
-                     />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password"> Password: </label>
-                <input 
-                    type="text" //explore type password later
-                    id="password" 
-                    value={ password }
-                    onChange={(event) => setPassword(event.target.value)}
-                    />
-            </div>
-            <button type="submit"> Register </button>
-        </form>
-    </div>
+    return (
+        <div className="auth-container">
+            <form>
+                <h2> Register </h2>
+                <div className="form-group">
+                    <label htmlFor="username"> Username: </label>
+                    <input
+                         type="text" 
+                         id="username"
+                         value = { username }
+                         onChange={(event) => setUsername(event.target.value)}
+                         />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password"> Password: </label>
+                    <input 
+                        type="text" // look into changing this to type="password" for security
+                        id="password" 
+                        value={ password }
+                        onChange={(event) => setPassword(event.target.value)}
+                        />
+                </div>
+                <button type="submit"> Register </button>
+            </form>
+        </div>
+    );
 }
