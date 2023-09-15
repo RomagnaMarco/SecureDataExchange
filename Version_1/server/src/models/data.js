c242b3e860abaeb273def8c1d351d3602ff78a6d
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-14 22:57:24 -0700

    changed some data fields

--- Version_1/server/src/models/data.js
+++ Version_1/server/src/models/data.js
@@ -3,15 +3,17 @@ import mongoose from "mongoose";
 
 // Defining a schema for the Data collection in MongoDB.
 // The schema describes the structure of the documents within this collection.
-const DataSchema = new mongoose.Schema({              
-    clearanceLevel: {type: Number, required: true},
-    info: [{ type: String, required: false }],
+const DataSchema = new mongoose.Schema({
+    clearanceLevel: { type: Number, required: true },
+    description: { type: String, required: true },
+    tags: [{ type: String, required: true }], // Array of strings for tags
+    info: { type: String, required: false },
     date: { type: Date, default: Date.now },
-    userOwner: { 
-        type: mongoose.Schema.Types.ObjectId,         // Using ObjectId to reference a user document.
-        ref: "users",                                // Establishing a reference to the 'users' collection.
-        required: true                               // Mandatory field to track the original user who added the data.
-    }
+    userOwner: {
+        type: mongoose.Schema.Types.ObjectId, // Using ObjectId to reference a user document.
+        ref: "users", // Establishing a reference to the 'users' collection.
+        required: true, // Mandatory field to track the original user who added the data.
+    },
 });
 
 // Creating a model named 'data' based on the DataSchema.
