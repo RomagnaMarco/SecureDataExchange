c2fc8f92629138035ee6e7f33d4ef53de55ac96c
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-15 22:03:04 -0700

    got rid of old code

--- Version_1/client/src/pages/add-data.js
+++ Version_1/client/src/pages/add-data.js
@@ -73,18 +73,6 @@ export const AddData = () => {
     }
   };
   
-  // Function to get a cookie by name
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) {
-      const token = parts.pop().split(';').shift();
-      return token.trim(); //remove leading/trailing whitespaces
-    }
-  }
-  
-  
-  
   // Function to decode the JWT token
   function decodeToken(token) {
     if (!token) {
