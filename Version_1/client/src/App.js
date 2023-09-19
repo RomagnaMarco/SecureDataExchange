13604f4a9370c9ddbe8e3abd04e602571df14af0
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-18 17:40:41 -0700

    update comments

--- Version_1/client/src/App.js
+++ Version_1/client/src/App.js
@@ -1,32 +1,38 @@
-// Importing necessary styles and components
+// Importing styles and component dependencies
 import './App.css';
-// React Router imports for setting up routing
+
+// React Router components for managing in-app navigation and routing
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// Importing page components
+
+// Importing individual page components to be routed
 import { Home } from './pages/home';
 import { SavedData } from './pages/saved-data';
 import { AddData } from './pages/add-data';
 import { Auth } from './pages/auth';
+
+// Importing shared Navbar component
 import { Navbar } from './components/navbar';
 
-// Main App component
+// Main App component which is the root component of the application
 function App() {
   return (
-    <div className="App"> 
-        {/* Setting up the router */}
+    <div className="App">
+        {/* Wrapping the entire app inside the Router component to enable routing */}
         <Router>
-          <Navbar />
-            {/* Defining routes for the application */}
+            {/* Global Navbar component, displayed on all pages */}
+            <Navbar />
+
+            {/* Route definitions mapping URL paths to corresponding page components */}
             <Routes>
-                <Route path="/" element={ <Home />} />
-                <Route path="/auth" element={ <Auth />} />
-                <Route path="/add-data" element={ <AddData />} />
-                <Route path="/saved-data" element={ <SavedData />} />
+                <Route path="/" element={<Home />} />              // Home route
+                <Route path="/auth" element={<Auth />} />          // Authentication route (Login/Register)
+                <Route path="/add-data" element={<AddData />} />   // Route to add new data
+                <Route path="/saved-data" element={<SavedData />} /> // Route to view saved data
             </Routes>
         </Router>
     </div>
   );
 }
 
-// Exporting the App component for use in other parts of the application
+// Export the App component so it can be used as the root component in the application's entry point
 export default App;
--- Version_1/client/src/components/navbar.js
+++ Version_1/client/src/components/navbar.js
@@ -1,44 +1,52 @@
-// Importing necessary libraries and hooks
+// Importing necessary libraries and hooks for routing, cookie management, and JWT decoding
 import { Link } from 'react-router-dom';
 import { useCookies } from "react-cookie";
 import { useNavigate } from 'react-router-dom';
-import jwtDecode from "jwt-decode";  // Import the JWT decoding library
+import jwtDecode from "jwt-decode";
 
 export const Navbar = () => {
-    // Using the useCookies hook to manage the access_token cookie
+    // Manage the access_token using the useCookies hook
     const [cookies, setCookies] = useCookies(["access_token"]);
 
-    // Using the useNavigate hook from react-router for programmatic navigation
+    // Use the useNavigate hook from react-router for programmatic navigation
     const navigate = useNavigate();
 
-    let clearanceLevel = 0; // Default clearance level
+    // Set the default clearance level for a user
+    let clearanceLevel = 0;
 
-    // If there's an access_token, decode it to get the clearance level
+    // Check for the presence of an access_token and decode it to obtain the user's clearance level
     if (cookies.access_token) {
         const decodedToken = jwtDecode(cookies.access_token);
-        clearanceLevel = decodedToken.clearanceLevel;
+        clearanceLevel = decodedToken.clearanceLevel || 0; // Extract clearance level or default to 0
     }
 
-    // Logout function: clears the access_token cookie, removes userID from local storage, and navigates to the auth page
+    /**
+     * Handles the logout process by:
+     * 1. Removing the access_token from cookies
+     * 2. Deleting the userID from local storage
+     * 3. Redirecting the user to the authentication page
+     */
     const logout = () => {
-        setCookies("access_token", ""); // Clearing the access_token cookie
-        window.localStorage.removeItem("userID"); // Removing the userID from local storage
-        navigate("/auth"); // Navigating to the auth page
+        setCookies("access_token", "", { expires: new Date(0) }); // Expire the access_token cookie immediately
+        window.localStorage.removeItem("userID"); // Remove userID from local storage
+        navigate("/auth"); // Redirect to the authentication page
     }
 
     return (
-        <div className="navbar"> 
-            {/* Navigation links */}
+        <div className="navbar">
+            {/* Standard link to the home page */}
             <Link to="/"> Home </Link>{" "}
             
-            {/* Conditional rendering based on whether the user is logged in (i.e., has an access_token) */}
+            {/* Display certain navigation links based on the user's authentication status and clearance level */}
             {cookies.access_token && (
                 <>
+                    {/* Show the "Add Data" link only to users with a clearance level of 1 or higher */}
                     {clearanceLevel >= 1 && <Link to="/add-data"> Add Data </Link>}{" "}
                     <Link to="/saved-data"> Saved Data </Link>{" "}
                 </>
             )}
 
+            {/* Conditionally display either a "Login/Register" link or a "Logout" button based on the user's authentication status */}
             {!cookies.access_token ? (
                 <Link to="/auth"> Login/Register </Link>
             ) : (
