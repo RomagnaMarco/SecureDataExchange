4b7f3dd25e4710be98feae1dc55aa1c6d9d3b3a9
RomagnaMarco <marco.romagna@yahoo.com>
2023-09-19 17:33:09 -0700

    revert to unbreak

--- Version_1/client/src/pages/auth.js
+++ Version_1/client/src/pages/auth.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from 'axios';
+
+// Import hooks for managing cookies and navigation
 import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 
+/**
+ * Main authentication component comprising both Login and Register forms.
+ */
 export const Auth = () => {
     return (
         <div className="auth">
@@ -12,22 +17,39 @@ export const Auth = () => {
     );
 }
 
+/**
+ * Component to handle user login.
+ */
 const Login = () => {
+    // State to manage login form fields
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    // Hook to set and get cookies; primarily used for the access token
     const [, setCookies] = useCookies(["access_token"]);
+
+    // Hook to navigate between routes programmatically
     const navigate = useNavigate();
 
+    /**
+     * Handler function for login form submission.
+     */
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
+            // Authenticate the user with the backend
             const response = await axios.post("http://localhost:3001/auth/login", {
                 username,
                 password,
             });
 
+            // Save the access token in a cookie upon successful authentication
             setCookies("access_token", response.data.token);
+
+            // Store the user's ID in local storage for later retrieval
             window.localStorage.setItem("userID", response.data.userID);
+
+            // Navigate the user to the homepage after successful login
             navigate("/");
         } catch(err) {
             alert("An error occurred during Login.");
@@ -35,6 +57,7 @@ const Login = () => {
         }
     }
 
+    // Render the Form component tailored for login
     return (
         <Form 
             username={username}
@@ -47,13 +70,21 @@ const Login = () => {
     );
 }
 
+/**
+ * Component to handle user registration.
+ */
 const Register = () => {
+    // State to manage registration form fields
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
+    /**
+     * Handler function for registration form submission.
+     */
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
+            // Register the user with the backend
             await axios.post("http://localhost:3001/auth/register", {
                 username,
                 password,
@@ -65,6 +96,7 @@ const Register = () => {
         }
     }
 
+    // Render the Form component tailored for registration
     return (
         <Form 
             username={username}
@@ -77,6 +109,9 @@ const Register = () => {
     );
 }
 
+/**
+ * Reusable Form component to cater to both Login and Registration.
+ */
 const Form = ({
     username,
     setUsername,
@@ -90,26 +125,29 @@ const Form = ({
             <form onSubmit={onSubmit}>
                 <h2>{label}</h2>
                 
+                {/* Input field for username */}
                 <div className="form-group">
                     <label htmlFor="username">Username:</label>
                     <input
                         type="text"
-                        id="login-username" 
+                        id="username"
                         value={username}
                         onChange={(event) => setUsername(event.target.value)}
                     />
                 </div>
-
+                
+                {/* Input field for password */}
                 <div className="form-group">
                     <label htmlFor="password">Password:</label>
                     <input
                         type="password"
-                        id="login-password" 
+                        id="password"
                         value={password}
                         onChange={(event) => setPassword(event.target.value)}
                     />
                 </div>
                 
+                {/* Submit button. Label (Login/Register) is determined by the parent component */}
                 <button type="submit">{label}</button>
             </form>
         </div>
